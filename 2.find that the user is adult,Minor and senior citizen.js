61d2b16eb8272ae79d621fdfc233c7b5237cbf64
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-31 01:19:42 +0530

    first commit

---
+++ 2.find that the user is adult,Minor and senior citizen.js
@@ -0,0 +1,12 @@
+let age=prompt("Enter Your Age");
+if(age<18)
+{
+    alert("You Are an Adult");
+}
+else if(age>=18 && age<=65)
+{
+    alert("You Are a Minor");
+}
+else{
+    alert("You Are a Senior Citizen");
+}
---
+++ 3.find number is positiv ,negative or zero.js
@@ -0,0 +1,12 @@
+let num=prompt("Enter the number");
+if(num<0)
+(
+    alert("Entered Number is Negative")
+)
+else if(num>0)
+{
+    alert("Entered Number is Positive")
+}
+else{
+    alert("Entered Number is Zero")
+}
